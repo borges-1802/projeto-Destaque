1304a808b6b3d1b7b517ca5cea4577c5022ac53c
Borges <jvbona98@gmail.com>
2025-11-23 16:55:03 -0300

    commit do código

--- frontend/src/services/api.tsx
+++ frontend/src/services/api.tsx
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-//const baseURL = process.env.REACT_APP_API_URL || "http://localhost:8000";
-// E no .env de produção/host: REACT_APP_API_URL=https://seu-backend.example.com
-
 const baseURL = "http://localhost:8000";
 
 const api = axios.create({
